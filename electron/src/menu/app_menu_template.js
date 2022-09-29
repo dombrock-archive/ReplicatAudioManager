96cdbaffa073492323ee333bf074c62c13852664
= <matdombrock@gmail.com>
2022-09-29 12:40:01 -0700

    include config file

--- electron/src/menu/app_menu_template.js
+++ electron/src/menu/app_menu_template.js
@@ -1,4 +1,4 @@
-import { app } from "electron";
+import { app, BrowserWindow } from "electron";
 
 export default {
   label: "App",
@@ -8,6 +8,20 @@ export default {
       accelerator: "CmdOrCtrl+Q",
       click: () => {
         app.quit();
+      }
+    },
+    {
+      label: "Reload",
+      accelerator: "CmdOrCtrl+R",
+      click: () => {
+        BrowserWindow.getFocusedWindow().webContents.reloadIgnoringCache();
+      }
+    },
+    {
+      label: "Toggle DevTools",
+      accelerator: "Alt+CmdOrCtrl+I",
+      click: () => {
+        BrowserWindow.getFocusedWindow().toggleDevTools();
       }
     }
   ]
